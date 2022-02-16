ade89334a6234ac12a0fc1b7f3132df373d45377
Deepakkewat05 <99481427+Deepakkewat05@users.noreply.github.com>
2022-02-16 19:14:03 +0530

    3.2

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var btnTranslate = document.querySelector("#btn-translate");
 
-console.log("btnTranslate")
+var txtInput = document querySelector("#txt-input");
+
+var outputDiv = document.querySelector("#output");
 
 
+
+function clickHandler() {
+
+outputDiv.innerText = "AAAbbb " + txtInput.value;
+};
 
 btnTranslate.addEventLidtener("click", function clickEventHandler(){
     console.log("CLicked")
