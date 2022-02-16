8f8f8ecd3c5edf6cb7fa4a834704d77ef114d3b6
Deepakkewat05 <99481427+Deepakkewat05@users.noreply.github.com>
2022-02-16 18:10:12 +0530

    2.0

---
+++ app.js
@@ -0,0 +1,19 @@
+var btnTranslate = document.querySelector("#btn-translate");
+
+console.log("btnTranslate")
+
+
+
+btnTranslate.addEventLidtener("click", function clickEventHandler(){
+    console.log("CLicked")
+})
+
+
+
+
+
+
+
+
+
+
